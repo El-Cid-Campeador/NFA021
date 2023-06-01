e3aaa422cb5224a8428c21c9a9771dce3e2bd3c3
Lenovo <jalal0akbar@gmail.com>
2023-06-01 16:30:57 +0300

    v0.0.0

--- server/ts/index.ts
+++ server/ts/index.ts
@@ -4,6 +4,7 @@ import crypto from 'node:crypto';
 import mysql from 'mysql2/promise';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import 'dotenv/config';
 
 const PORT = 8080;
 const app = express();
@@ -12,47 +13,47 @@ async function connectDB() {
     const db = await mysql.createConnection({ /** */
         host: '127.0.0.1',
         user: 'root',
-        password: 'Ur@nium235',
+        password: process.env.DB_PASSWORD!,
         database: 'nfa021'
     });
 
     await db.execute(`CREATE TABLE IF NOT EXISTS User (
-            id TEXT PRIMARY KEY,
-            firstName TEXT NOT NULL,
-            lastName TEXT NOT NULL,
-            email TEXT NOT NULL,
-            password TEXT NOT NULL,
+            id VARCHAR(255) PRIMARY KEY,
+            firstName VARCHAR(255) NOT NULL,
+            lastName VARCHAR(255) NOT NULL,
+            email VARCHAR(255) NOT NULL,
+            password VARCHAR(255) NOT NULL,
             isMember INT NOT NULL
         )`
     );
 
     await db.execute(`CREATE TABLE IF NOT EXISTS Book (
-            id TEXT PRIMARY KEY,
-            title TEXT NOT NULL,
-            author TEXT NOT NULL,
-            descr TEXT NOT NULL,
+            id VARCHAR(255) PRIMARY KEY,
+            title VARCHAR(255) NOT NULL,
+            author VARCHAR(255) NOT NULL,
+            descr VARCHAR(255) NOT NULL,
             yearPubl INT NOT NULL,
             numEdition INT NOT NULL,
-            idMember TEXT,
+            idMember VARCHAR(255),
             FOREIGN KEY(idMember) REFERENCES User(id)
         )`
     );
 
     await db.execute(`CREATE TABLE IF NOT EXISTS Fees (
-            id TEXT PRIMARY KEY,
+            id VARCHAR(255) PRIMARY KEY,
             amount FLOAT NOT NULL,
             date INT NOT NULL,
-            idMember TEXT NOT NULL,
+            idMember VARCHAR(255) NOT NULL,
             FOREIGN KEY(idMember) REFERENCES User(id)
         )`
     );
 
     await db.execute(`CREATE TABLE IF NOT EXISTS Suggestion (
-            id TEXT PRIMARY KEY,
-            descr TEXT NOT NULL,
+            id VARCHAR(255) PRIMARY KEY,
+            descr VARCHAR(255) NOT NULL,
             date INT NOT NULL,
-            idMember TEXT NOT NULL,
-            idBook TEXT NOT NULL,
+            idMember VARCHAR(255) NOT NULL,
+            idBook VARCHAR(255) NOT NULL,
             FOREIGN KEY(idMember) REFERENCES User(id),
             FOREIGN KEY(idBook) REFERENCES Book(id)
         )`
@@ -66,75 +67,118 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.post('/signup', async(req, res) => {
-    const id = crypto.randomUUID();
+    const tokenVerif = req.headers['x-auth-token'];
     const { firstName, lastName, email, password } = req.body;
+
+    async function isMember() {
+        if (tokenVerif) {
+            const decoded = jwt.verify(String(tokenVerif), process.env.JWT_SECRET_KEY!) as { id: string, email: string, hash: string, isMember: number };
+
+            if (decoded) {
+                const { email, hash } = decoded;
+                if (email && hash) {
+                    const stmt = await conn.prepare(`SELECT * FROM User WHERE email = ?`);
+                    const rows = await stmt.execute([email]) as any[][];
+            
+                    if (rows[0].length) {
+                        const { password: hash, isMember } = rows[0][0];
+                        
+                        if (bcrypt.compareSync(password, hash)) {
+                            if (isMember === 0) {
+                                return 0;
+                            }
+                        }        
+                    }
+                
+                    await stmt.close();
+                }
+            }
+        }
+
+        return 1;
+    };
+
     const conn = await connectDB();
     const salt = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync(password, salt);
 
-    await conn.execute(`INSERT INTO User (id, firstName, lastName, email, password, isMember) VALUES (?, ?, ?, ?, ?, 1)`, (err: Error, stmt: mysql.PrepareStatementInfo) => {
-        stmt.execute([crypto.randomUUID(), firstName, lastName, email, hash]);
-        stmt.close();
-    });
+    const stmt = await conn.prepare(`INSERT INTO User (id, firstName, lastName, email, password, isMember) VALUES (?, ?, ?, ?, ?, ?)`);
+    await stmt.execute([crypto.randomUUID(), firstName, lastName, email, hash, isMember()]);
+    await stmt.close();
 
     await conn.end();
     
-    res.json({ });
+    res.json({ msg: 'Successfully registred!' });
 });
 
 app.post('/login', async(req, res) => {
-    const { email, password } = req.body;
+    let { email, password } = req.body;
     const conn = await connectDB();
-    const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync(password, salt);
+    
+    let msg = 'Error!';
 
-    await conn.execute(`SELECT * FROM User WHERE email = ? AND password = ?`, (err: Error, stmt: mysql.PrepareStatementInfo) => {
-        stmt.execute([email, hash], (err, rows) => {
-            if (rows) {
+    const tokenVerif = req.headers['x-auth-token'];
 
-            } else {
-                // j
-            }
-        });
-        stmt.close();
-    });
+    if (tokenVerif) {
+        const decoded = jwt.verify(String(tokenVerif), process.env.JWT_SECRET_KEY!) as { id: string, email: string, hash: string, isMember: number };
+        
+        if (decoded) {
+            email = decoded.email;
+            password = decoded.hash;
+        }
+    }
+
+    if (email && password) {
+        const stmt = await conn.prepare(`SELECT * FROM User WHERE email = ?`);
+        const rows = await stmt.execute([email]) as any[][];
+
+        if (rows[0].length) {
+            const { id, email, password: hash, isMember } = rows[0][0];
+            
+            if (bcrypt.compareSync(password, hash)) {
+                msg = jwt.sign({ id, email, hash, isMember }, process.env.JWT_SECRET_KEY!, { expiresIn: '24h' });
+            }        
+        }
+    
+        await stmt.close();
+    }
 
     await conn.end();
 
-    res.json({ msg: '' });
+    res.json({ msg: msg });
 });
 
-app.route('/books')
-    .get(async(req, res) => {
-        res.json({ result: [] });
-    })
-    .post(async(req, res) => {
+// app.route('/books')
+//     .get(async(req, res) => {
+//         res.json({ result: [] });
+//     })
+//     .post(async(req, res) => {
 
-    });
+//     });
 
-app.route('/book/:id')
-    .get(async(req, res) => {
-        const { id } = req.params;
-        res.json({ result: '' });
-    })
-    .patch(async(req, res) => {
-        const { id } = req.params;
-        res.json({ msg: '' });
-    })
-    .delete(async(req, res) => {
-        const { id } = req.params;
-        res.json({ msg: '' });
-    });
+// app.route('/book/:id')
+//     .get(async(req, res) => {
+//         const { id } = req.params;
+//         res.json({ result: '' });
+//     })
+//     .patch(async(req, res) => {
+//         const { id } = req.params;
+//         res.json({ msg: '' });
+//     })
+//     .delete(async(req, res) => {
+//         const { id } = req.params;
+//         res.json({ msg: '' });
+//     });
 
-app.route('book/:id/suggest')
-    .get(async(req, res) => {
-        const { id } = req.params;
-        res.json({ result: '' });
-    })
-    .post(async(req, res) => {
-        const { id } = req.params;
-        res.json({ msg: '' });
-    });
+// app.route('book/:id/suggest')
+//     .get(async(req, res) => {
+//         const { id } = req.params;
+//         res.json({ result: '' });
+//     })
+//     .post(async(req, res) => {
+//         const { id } = req.params;
+//         res.json({ msg: '' });
+//     });
 
 app.listen(PORT, () => {
     console.log(`At http://127.0.0.1:${PORT}`);
